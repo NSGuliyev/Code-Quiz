56d762fdfbe2f9119adec140c1106f4af5449891
NSGuliyev <nesimi018@yahoo.com>
2019-12-10 00:18:40 -0500

    JavaScript is ready

--- Assets/JavaScript/questions.js
+++ Assets/JavaScript/questions.js
@@ -1,31 +1,27 @@
 var questions = [
     {
-        title: "George Washington was the _______ President of the United States?",
+        title: "George Washington was the ______ President of the United States ?",
         choices: ["Sixteenth", "Third", "First", "Fourth"],
         answer: "First"
     },
-
     {
         title: "Who was George Washington's wife?",
-        choices: ["Martha Dandridge", "Abigail Smith", "Martha Wayles Skelton", "Dolly Payne Todd"],
-        answer: "Martha Dandridge"
+        choices: ["Martha Dandrige", "Abigal Smith", "Martha Wayles Skelton", "Dolly Payne Todd"],
+        answer: "Martha Dandrige"
     },
-
     {
-        title: "Who was the Vice President of the United States under George Washington?",
+        title: "Who was the Vice President of the Unidet States under George Washington?",
         choices: ["George Clinton", "Thomas Jefferson", "Aaron Burr", "John Adams"],
         answer: "John Adams"
     },
-
     {
         title: "What was George Washington's nickname?",
-        choices: [ "Father of the Declaration of Independence", "Father of the Constitution", "The Era of Good Feelings President", "The Father of His Country"],
+        choices: ["Father of the Declaration of Independence", "Father of the Constitution", "The Era of Good Feelings President", "The Father of His Country"],
         answer: "The Father of His Country"
     },
-
     {
         title: "Where was George Washington born?",
-        choices: [ "North Carolina", "Virginia", "Ohio", "New York"],
-        answer: " Virginia"
+        choices: ["North Carolina", "Virginia", "Ohio", "New York"],
+        answer: "Virginia"
     }
 ];
--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,15 +1,15 @@
 var questionBtns = document.querySelector(".buttonQuestions .col-sm-3");
-listOfScoresDiv = document.querySelector(".listOfScore");
-startBtn = document.querySelector(".starBtn");
+listOfScoresDiv = document.querySelector(".listOfScores");
+startBtn = document.querySelector(".startBtn");
 questionDiv = document.getElementById("questionDiv");
 addInitialsId = document.getElementById("addInitialsId");
-highScoreDiv = document.getElementById("highScoreWrapper");
+highScoresDiv = document.getElementById("highScoreWrapper");
 scoreWrapper = document.getElementById("scoreWrapper");
 goBackBtn = document.getElementById("goBackBtn");
 clearBtn = document.getElementById("clearBtn");
-viewHighScoreBtn = document.getElementById("viewHighScoreBtn");
+viewHighscoreBtn = document.getElementById("viewHighscoreBtn");
 timerSpan = document.getElementById("timerSpan");
-answerCheckDiv = document.getElementById("answerCheckDiv");
+answerCheckDiv = document.getElementById("answerCheckDiv")
 highScoreItemsDiv = document.querySelector(".listOfScores");
 
 var highScores = [];
@@ -20,25 +20,23 @@ timeElapsed = 0;
 var time;
 
 
-// Seting up event listeners
+//-------------------------------------------------------------all event listeners
 
 startBtn.addEventListener("click", function (event) {
     event.preventDefault()
     startBtn.classList.add("hideClass")
     questionBtns.parentElement.classList.remove("hideClass")
-    creatBtns();
+    createBtns();
     startTimer();
 });
-
 
 questionBtns.addEventListener("click", function (event) {
     event.stopPropagation();
     checkAnswer(event);
 
-    if (questionIndex == question.length) {
+    if (questionIndex == questions.length) {
         displayScore();
-    }
-    else {
+    } else {
         createBtns(event);
     }
     return
@@ -52,19 +50,150 @@ addInitialsId.addEventListener("click", function (e) {
 
 goBackBtn.addEventListener("click", function (e) {
     location.reload();
-
 });
 
 clearBtn.addEventListener("click", function (e) {
-    highScoreItemsDiv.innerHtml = "";
+    highScoreItemsDiv.innerHTML = "";
     localStorage.clear();
     highScores = [];
 });
 
-viewHighScoreBtn.addEventListener("click", function (e) {
+viewHighscoreBtn.addEventListener("click", function (e) {
     startBtn.classList.add("hideClass")
     viewHighScores();
 });
 
 
-// Button functionality settings
+
+//--------------------------------------------------------button functionality
+
+function createBtns(event) {
+    questionBtns.innerHTML = "";
+    questionDiv.innerHTML = "";
+
+    questionDiv.innerHTML = "<h5>" + questions[questionIndex].title + "</h5>";
+
+    for (var i = 0; i < questions[questionIndex].choices.length; i++) {
+        var listItem = document.createElement('span');
+        listItem.innerHTML = "<button type='button' class='btn btn-dark btn-block mb-2' >" + questions[questionIndex].choices[i] + "</button>";
+        questionBtns.appendChild(listItem);
+    }
+    questionIndex++;
+    return
+};
+
+function checkAnswer(event) {
+    answerCheckDiv.innerHTML = "";
+    
+    var listItem = "";
+    var listItem = document.createElement('div');
+    var key;
+
+    if (event.target.innerHTML === questions[questionIndex - 1].answer) {
+        key = "right";
+        correctAnswers++;
+        console.log(correctAnswers, "checkanswer");
+    } else {
+        key = "wrong !";
+        timeElapsed = timeElapsed + 15;
+    }
+
+    answerCheckDiv.style.display = "block";
+    var op = 1;
+    var timerFade = setInterval(function(){
+        if(op <= 0.1){
+            clearInterval(timerFade);
+            answerCheckDiv.style.display = "none";
+        }
+        answerCheckDiv.style.opacity = op;
+        answerCheckDiv.style.filter = "alpha(opacity=" + op * 100 + ")";
+        op-= op * 0.1;
+    }, 50);
+
+
+    listItem.innerHTML = key;
+    answerCheckDiv.appendChild(listItem);
+    return
+};
+
+
+
+
+// ---------------------------------------------------- creating scores functions
+function displayScore() {
+    document.getElementById("questionsWrapper").classList.add("hideClass");
+    document.getElementById("scoreWrapper").classList.remove("hideClass");
+    document.getElementById("answerId").innerHTML = " " + correctAnswers + " ";
+    clearInterval(time);
+
+}
+
+function viewHighScores() {
+    scoreWrapper.classList.add("hideClass");
+    highScoresDiv.classList.remove("hideClass");
+    document.getElementById("questionsWrapper").classList.add("hideClass");
+    clearInterval(time);
+    timerSpan.innerHTML = "00:00"
+    highScoreItemsDiv.innerHTML = "";
+
+    highScores = JSON.parse(localStorage.getItem("scores"));
+    if (localStorage.getItem("scores") !== null) {
+        for (var i = 0; i < highScores.length; i++) {
+            var item = document.createElement("p");
+            item.innerHTML = highScores[i].name + " - " + highScores[i].score + " points";
+            highScoreItemsDiv.appendChild(item);
+        }
+    }
+
+
+}
+
+function storeScores() {
+    var intialsInput = document.getElementById("intialsInput").value;
+    var scores;
+    var calculatedScore = Math.max(0, timerTotal - timeElapsed) * correctAnswers;
+    var temp = { "name": intialsInput, "score": calculatedScore };
+
+    if (localStorage.getItem("scores") !== null) {
+        highScores = JSON.parse(localStorage.getItem("scores"));
+    }
+    highScores.push(temp);
+
+    scores = JSON.stringify(highScores);
+    localStorage.setItem("scores", scores);
+    //highScores = [];
+    return
+}
+
+
+//-------------------------------------------------------------timer functions
+function startTimer() {
+    time = setInterval(function () {
+        var totalSecondsLeft = timerTotal - timeElapsed;
+        var minutesLeft = Math.floor(totalSecondsLeft / 60);
+        var secondsLeft = totalSecondsLeft % 60;
+        var formattedSeconds;
+        var formattedMinutes;
+
+        if (timeElapsed > timerTotal) {
+            clearInterval(time);
+            timerSpan.innerHTML = "No more time left!"
+            displayScore();
+            return
+        }
+        timeElapsed++;
+        if (secondsLeft < 10) {
+            formattedSeconds = "0" + secondsLeft;
+        } else {
+            formattedSeconds = secondsLeft;
+        }
+
+        if (minutesLeft < 10) {
+            formattedMinutes = "0" + minutesLeft;
+        } else {
+            formattedMinutes = minutesLeft;
+        }
+
+        timerSpan.innerHTML = formattedMinutes + ":" + formattedSeconds;
+    }, 1000)
+}
